aeb667d8ee683fe610a28c73e9f05508f8cb44fa
Ben1729 <huck.ben@gmail.com>
2020-10-21 13:45:32 -0400

    commit

--- codebreaker2020/task6.py
+++ codebreaker2020/task6.py
@@ -2,18 +2,20 @@ import numpy as np
 import struct, math
 import matplotlib.pyplot as plt
 
-#hamming check matrix
-H = np.array(  [[0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0],
-                [0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0],
-                [0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0],+
-                [0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0]])
-
-#[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]])
+H=np.array([[0,1,0,1,0,1,1,1,0,1,1,1,0,0,0,0,0],
+            [1,1,1,0,0,0,0,1,1,1,1,0,1,0,0,0,0],
+            [1,1,0,0,1,1,1,0,1,1,0,0,0,1,0,0,0],
+            [1,1,1,1,1,1,0,0,0,0,1,0,0,0,1,0,0],
+            [0,1,1,1,1,0,1,1,1,0,0,0,0,0,0,1,0]])
 
 #returns the int array for a binary string
 def toIndex(string):
-    dic = {'0000': -1, '1100': 0, '1010': 1, '0110': 2, '1110': 3, '1001': 4, '0101': 5, '1101': 6, '0011': 7, '1011': 8, '0111': 9, '1111': 10}
     try:
+        dic = {'00000': -1, '01110': 0, '11111': 1, '01011': 2, '10011': 3, '00111': 4, '10110': 5,
+        '10101': 6, '11001': 7, '01101': 8, '11100': 9, '11010': 10, '10000': 11, '01000': 12,
+        '00100': 13, '00010': 14, '00001': 15}
+        #print(string)
+        #input()
         return dic[string]
     except:
         return -1
@@ -37,16 +39,48 @@ def hammingDecode(b):
 
     return b[:11]
 
-
+def thing(bitsArray):
+    
+    words = {}
+    for vector in bitsArray:
+        words[vector] = int(words.setdefault(vector, 0)) + 1
+    for key in words:
+        val = words[key]
+        if val > 20:
+            if  key[:11] == '10000000000':
+                print(key, words[key])
+            if  key[:11] == '01000000000':
+                print(key, words[key])
+            if  key[:11] == '00100000000':
+                print(key, words[key])
+            if  key[:11] == '00010000000':
+                print(key, words[key])
+            if  key[:11] == '00001000000':
+                print(key, words[key])
+            if  key[:11] == '00000100000':
+                print(key, words[key])
+            if  key[:11] == '00000010000':
+                print(key, words[key])
+            if  key[:11] == '00000001000':
+                print(key, words[key])
+            if  key[:11] == '00000000100':
+                print(key, words[key])
+            if  key[:11] == '00000000010':
+                print(key, words[key])
+            if  key[:11] == '00000000001':
+               print(key, words[key])
+    
+    
 bits = ''
 bitsArray = []
 vallarr = []
+
 
 with open('signal.ham', 'rb') as file:
 
     #file.read(145250)
     #for x in range(131072):
-    #for x in range(512):
+    #for x in range(2048):
     while True:
         try:
             #import floats from file
@@ -71,40 +105,35 @@ with open('signal.ham', 'rb') as file:
             #print(bits)
             #input()
             bits = ''
-
-    words = {}
+    #thing(bitsArray)
+    
     with open('out.avi', 'ab') as file:
         byteBuilder = ''
         #print('|data bits  |parity|pad|')
         for vector in bitsArray:
+            #print(vector)
             error = toIndex(getError(vector))
             word = hammingDecode(vector[:17])
             if error != -1:
                 
-                byteBuilder += word
+                #yteBuilder += word
 
-                #if vector[error] == '1': 
-                #    byteBuilder += hammingDecode(vector[:error] + '0' + vector[error:])
-                #else: 
-                #    byteBuilder += hammingDecode(vector[:error] + '1' + vector[error:])
+                if vector[error] == '1': 
+                    byteBuilder += hammingDecode(vector[:error] + '0' + vector[error:])
+                else: 
+                    byteBuilder += hammingDecode(vector[:error] + '1' + vector[error:])
 
             else:
                 byteBuilder += word
             
             #print('|' + str(word) + '|' + str(vector[11:16]) + ' |' + str(vector[16]) + '  |')
             #words[vector] = words.setdefault(vector, 1) + 1
-        #with open('test', 'w') as testOut:
-         #   for key in words:
-          #      val = words[key]
-           #     if val > 1000:
-            #        print('\'' + key + '\', ')
-             #       testOut.write(str(key) + ' - ' + str(val) + '\n')
         print('almost done')
         while True:
          #   try:
             s = byteBuilder[:16]
             byteBuilder = byteBuilder[16:]
-            #print(s, int(s, 2).to_bytes(2, 'big'))
+            #print(int(s, 2).to_bytes(2, 'big'))
             file.write(int(s, 2).to_bytes(2, 'big'))
         #except:
                 
